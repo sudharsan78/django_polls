942f748a3bed083cd8466f58bfa5be4fd7eeb31b
sudharsan78 <sudharsan@testpress.in>
2016-04-05 16:10:28 +0530

    Create models and admin
    Created models for question and choice and linked both to the admin.

---
+++ polls/migrations/0003_choice_votes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('polls', '0002_choice_choice_text'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='choice',
+            name='votes',
+            field=models.IntegerField(default=0),
+        ),
+    ]
